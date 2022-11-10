19f81f8502207704810f361419f0f72c12042235
__alihejrati__ <alihejrati1996@gmail.com>
2022-11-10 16:49:35 +0330

    bug fix

--- apps/VQGAN/modules/dataset.py
+++ apps/VQGAN/modules/dataset.py
@@ -1,3 +1,5 @@
+from apps.VQGAN.data.utils import custom_collate
+from apps.VQGAN.modules.configuration import Config
 from utils.ptDataset import DataModuleFromConfigBase, WrappedDatasetBase
 
 class WrappedDataset(WrappedDatasetBase):
@@ -6,5 +8,7 @@ class WrappedDataset(WrappedDatasetBase):
 class DataModuleFromConfig(DataModuleFromConfigBase):
     def __init__(self, **kwargs):
         print('####################', kwargs)
+        kwargs['custom_collate'] = custom_collate
+        kwargs['instantiate_from_config'] = Config.instantiate_from_config
         super().__init__(**kwargs)
         self.wrap_cls = WrappedDataset
