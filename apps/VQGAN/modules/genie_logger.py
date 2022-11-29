da0f75d20f6955e7fa999ddb32f83b1b38384064
__alihejrati__ <alihejrati1996@gmail.com>
2022-11-29 20:38:35 +0330

    bugfix

--- apps/VQGAN/modules/genie_logger.py
+++ apps/VQGAN/modules/genie_logger.py
@@ -1,4 +1,6 @@
 from utils.ptLogger import GenieLoggerBase
+from typing import Any, Callable, Dict, Mapping, Optional, Sequence, Union
 
 class GenieLogger(GenieLoggerBase):
-    pass
+    def __init__(self, save_dir: str, name: Optional[str] = 'GeineLogs', agg_key_funcs: Optional[Mapping[str, Callable[[Sequence[float]], float]]] = None, agg_default_func: Optional[Callable[[Sequence[float]], float]] = None, **kwargs: Any):
+        super().__init__(save_dir, name, agg_key_funcs, agg_default_func, **kwargs)
