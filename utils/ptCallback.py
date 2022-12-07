7275fd06e33c9f7c6991f6642e877b7c3cf06880
__alihejrati__ <alihejrati1996@gmail.com>
2022-12-07 22:37:11 +0330

    bugfix

--- utils/ptCallback.py
+++ utils/ptCallback.py
@@ -23,14 +23,13 @@ class ModelCheckpointBase(ModelCheckpointBasic):
 
 
 class CheckpointHandlerBase(Callback):
-    def on_save_checkpoint(self, *args, **kwargs):
-        print(type(a) for a in args)
-        print(k for k,v in kwargs.items())
+    def on_save_checkpoint(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", checkpoint: Dict[str, Any]):
         # checkpoint['something_cool_i_want_to_save'] = my_cool_pickable_object
-        # logger.warning('checkpoint.keys={}'.format(list(checkpoint.keys())))
+        logger.warning('checkpoint.keys={}'.format(list(checkpoint.keys())))
 
-    def on_load_checkpoint(self, checkpoint):
+    def on_load_checkpoint(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", callback_state: Dict[str, Any]):
         # my_cool_pickable_object = checkpoint['something_cool_i_want_to_save']
+        logger.warning('callback_state={}'.format(list(callback_state.keys())))
         pass
 
 class SetupCallbackBase(Callback):
