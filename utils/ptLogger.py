3a76662f60a3f7abb04665a20cb735505c19ed85
__alihejrati__ <alihejrati1996@gmail.com>
2022-12-10 02:33:02 +0330

    bugfix

--- utils/ptLogger.py
+++ utils/ptLogger.py
@@ -41,7 +41,7 @@ class GenieLoggerBase(Logger):
             for di in ['timestamp']:
                 cols.remove(di)
                 reconstructrd_hash = sha1(' | '.join(set(list(cols))))
-                # self.all_metrics_tbls[reconstructrd_hash] = self.create_metrics_table(list(cols))
+                self.all_metrics_tbls[reconstructrd_hash] = self.create_metrics_table(list(cols))
 
         self.inflect_engine = inflect.engine()
         self.flag_lock = True
