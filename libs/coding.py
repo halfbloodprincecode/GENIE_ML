20a54eb0c67b63090a81054f04a786dea32d5cd7
__alihejrati__ <alihejrati1996@gmail.com>
2022-12-10 16:18:29 +0330

    bugfix

--- libs/coding.py
+++ libs/coding.py
@@ -6,22 +6,24 @@ sha256 = lambda s: hashlib.sha256(s.encode('utf-8')).hexdigest()
 
 
 if __name__ == '__main__':
-    d = {
-        'name': 'ali',
-        'ok': 'okkk',
-        'age': 16
-    }
-    d2 = {
-        'age': 161,
-        'name': 'ali1',
-        'ok': 'okkk1',
-    }
-    s1 = ' | '.join((list(d.keys())))
-    s2 = ' | '.join((list(d2.keys())))
-    print('{} -> {}'.format(s1, sha1(s1)))
-    print('{} -> {}'.format(s2, sha1(s2)))
-    print('='*30)
-    s1 = ' | '.join(set(list(d.keys())))
-    s2 = ' | '.join(set(list(d2.keys())))
-    print('{} -> {}'.format(s1, sha1(s1)))
-    print('{} -> {}'.format(s2, sha1(s2)))
+    # d = {
+    #     'name': 'ali',
+    #     'ok': 'okkk',
+    #     'age': 16
+    # }
+    # d2 = {
+    #     'age': 161,
+    #     'name': 'ali1',
+    #     'ok': 'okkk1',
+    # }
+    # s1 = ' | '.join((list(d.keys())))
+    # s2 = ' | '.join((list(d2.keys())))
+    # print('{} -> {}'.format(s1, sha1(s1)))
+    # print('{} -> {}'.format(s2, sha1(s2)))
+    # print('='*30)
+    # s1 = ' | '.join(set(list(d.keys())))
+    # s2 = ' | '.join(set(list(d2.keys())))
+    # print('{} -> {}'.format(s1, sha1(s1)))
+    # print('{} -> {}'.format(s2, sha1(s2)))
+
+    print(sha1(set(['val/aeloss_step', 'val/discloss_step', 'val/total_loss_step', 'val/quant_loss_step', 'val/nll_loss_step', 'val/rec_loss_step', 'val/p_loss_step', 'val/d_weight_step', 'val/disc_factor_step', 'val/g_loss_step', 'val/disc_loss_step', 'val/logits_real_step', 'val/logits_fake_step', 'epoch'])))
