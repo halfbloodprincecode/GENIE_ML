1e5c1b0cc8a5efed03fed915ea27da25ce2c6053
__alihejrati__ <alihejrati1996@gmail.com>
2022-12-13 13:36:32 +0330

    bugfix

--- settings.py
+++ settings.py
@@ -58,7 +58,7 @@ environ['GENIE_ML_APP_FN'] = opt.app_fn
 environ['GENIE_ML_APP_DSC'] = opt.app_dsc
 
 if opt.stg0:
-    environ['GENIE_ML_STORAGE0'] = join(opt.stg0, environ['GENIE_ML_APP_DSC']) 
+    environ['GENIE_ML_STORAGE0'] = opt.stg0 #join(opt.stg0, environ['GENIE_ML_APP_DSC']) 
 
 PFX_KEYS = environ['GENIE_ML_PFX'].split(',')
 
